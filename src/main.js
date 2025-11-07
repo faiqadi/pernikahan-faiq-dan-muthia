29ae3d3c943dd2754292588b1c30b57fd74b0bb4
Faiq Adi <faiq.adiwibowo@gmail.com>
2025-11-07 13:55:30 +0700

    adjust bg

--- src/main.js
+++ src/main.js
@@ -7,6 +7,16 @@ import {navbar} from "./js/navbar.js";
 import {welcome} from "./js/welcome.js";
 import {story} from "./js/story.js";
 
+if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    document.querySelector('.bride').classList.add('ios-device');
+}
+if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    document.querySelector('.time').classList.add('ios-device');
+}
+if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    document.querySelector('.wishas').classList.add('ios-device');
+}
+
 // load content
 document.addEventListener('DOMContentLoaded', () => {
     AOS.init();
